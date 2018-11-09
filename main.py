9aea98156f911ae7cffde510df33f7eace7b9ee4
Milan Shah <shah.milan34@gmail.com>
2018-11-09 15:21:19 +0530

    Modified code according to style guide for python code(PEP 8)

--- main.py
+++ main.py
@@ -1,35 +1,5 @@
-from display import Display
-from movies import Movies
+from bookmyshow import BookMyShow
 
-class BookMyShow(Display):
-        
-    def run(self):
-        print("\n Welcome to BookMyShow \n \n Please select Movie from following list: \n\n")
-        movie = Movies()
-        
-        #Display movie list and select movie
-        movieList = movie.getMovies()
-        movie_id = super(BookMyShow,self).displayMovies(movieList)
-        
-        #Display date list and select date
-        movieDates = movie.getMovieDates(movie_id)
-        movies_dates_id = super(BookMyShow,self).displayDates(movie_id,movieList, movieDates)
-        
-        #Display time list and select time
-        movieTimes = movie.getMovieDateTimes(movie_id,movies_dates_id)
-        movies_time_id = super(BookMyShow,self).displayTimes(movie_id, movies_dates_id, movieList, movieDates, movieTimes)
-        
-        #Display seat nos and select seat
-        bookedTicketNo = movie.getBookedSeats(movie_id, movies_dates_id, movies_time_id)
-        seat_nos = super(BookMyShow,self).displaySeats(bookedTicketNo)
-        
-        decision = input("Want to Book Ticket \n Plese enter Y or N:")
-        if(decision == 'Y'):
-            for seat_no in seat_nos:
-                movie.bookTicket(movie_id,movies_dates_id,movies_time_id,seat_no,1)
-                print("\n Booked seat no "+str(seat_no)+" for "+movieList[movie_id]+" movie on date "+str(movieDates[movies_dates_id])+" and time "+str(movieTimes[movies_time_id])+" Successfully ")
-            
-        
 
 bookTicket = BookMyShow()
 bookTicket.run()
--- movies.py
+++ movies.py
@@ -1,50 +1,63 @@
 from dbconnection import DBConnection
 
+
 class Movies(DBConnection):
-       
-    #get movie list
+
+    # get movie list
     def getMovies(self):
         DBConnection.dictCursor.execute("SELECT * FROM movies")
         resultSet = DBConnection.dictCursor.fetchall()
         movie = {}
         for row in resultSet:
-            movie[row['id']]= row['name']
+            movie[row['id']] = row['name']
         return movie
-    
-    #get dates of movie
-    def getMovieDates(self,movies_id):
-        DBConnection.dictCursor.execute("SELECT id,date FROM movie_dates where movies_id="+str(movies_id))
+
+    # get dates of movie
+    def getMovieDates(self, movies_id):
+        DBConnection.dictCursor.execute(
+            "SELECT id,date FROM movie_dates where movies_id="+str(movies_id))
         resultSet = DBConnection.dictCursor.fetchall()
         movieDates = {}
         for row in resultSet:
-            movieDates[row['id']]= row['date']
+            movieDates[row['id']] = row['date']
         return movieDates
-    
-    #get time of movie on selected date
-    def getMovieDateTimes(self,movies_id,movies_dates_id):
-        DBConnection.dictCursor.execute("SELECT id,time FROM movie_times where movies_id="+str(movies_id)+" AND movies_dates_id ="+str(movies_dates_id))
+
+    # get time of movie on selected date
+    def getMovieDateTimes(self, movies_id, movies_dates_id):
+        DBConnection.dictCursor.execute(
+                "SELECT id,time FROM movie_times "
+                + "where movies_id="+str(movies_id)
+                + " AND movies_dates_id ="+str(movies_dates_id)
+            )
         resultSet = DBConnection.dictCursor.fetchall()
         movieDates = {}
         for row in resultSet:
-            movieDates[row['id']]= row['time']
+            movieDates[row['id']] = row['time']
         return movieDates
-    
-    #book ticket
-    def bookTicket(self, movies_id, movies_dates_id, movie_times_id, seat_no, user_id):
-        sql = "INSERT INTO tickets_booked (`movies_id`, `movies_dates_id`, `movie_times_id`, `seat_no`, `user_id`) VALUES (%s,%s,%s,%s,%s)"
+
+    # book ticket
+    def bookTicket(
+            self, movies_id, movies_dates_id,
+            movie_times_id, seat_no, user_id
+    ):
+        sql = "INSERT INTO tickets_booked (`movies_id`, `movies_dates_id`,"
+        sql += " `movie_times_id`, `seat_no`, `user_id`)"
+        sql += " VALUES (%s,%s,%s,%s,%s)"
         val = (movies_id, movies_dates_id, movie_times_id, seat_no, user_id)
         try:
-            DBConnection.dictCursor.execute(sql,val)
+            DBConnection.dictCursor.execute(sql, val)
             DBConnection.db.commit()
-        except:
+        except MySQLdb.error:
             DBConnection.db.rollback()
-            
-        #get time of movie on selected date
-    def getBookedSeats(self,movies_id,movies_dates_id,movie_times_id):
-        DBConnection.dictCursor.execute("SELECT seat_no FROM tickets_booked where movies_id="+str(movies_id)+" AND movies_dates_id ="+str(movies_dates_id)+" AND movie_times_id ="+str(movie_times_id))
+
+        # get time of movie on selected date
+    def getBookedSeats(self, movies_id, movies_dates_id, movie_times_id):
+        DBConnection.dictCursor.execute(
+            "SELECT seat_no FROM tickets_booked where movies_id="
+            + str(movies_id)+" AND movies_dates_id ="+str(movies_dates_id)
+            + " AND movie_times_id ="+str(movie_times_id))
         resultSet = DBConnection.dictCursor.fetchall()
         bookedSeats = []
         for row in resultSet:
             bookedSeats.append(row['seat_no'])
         return bookedSeats
-            
